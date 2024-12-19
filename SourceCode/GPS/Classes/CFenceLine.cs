d46047d41f4e5fc4740d105265ae6d767f989d6b
Ty-ios <louisbuchacker@gmail.com>
2024-12-19 22:40:05 +0100

    Update CFenceLine.cs
    changde some color all marked with // change ...

--- SourceCode/GPS/Classes/CFenceLine.cs
+++ SourceCode/GPS/Classes/CFenceLine.cs
@@ -1,174 +1,144 @@
-﻿using System;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
 
 namespace AgOpenGPS
 {
-    public partial class CBoundaryList
+    public partial class CBoundary
     {
-        //area variable
-        public double area;
+        public List<vec3> bndBeingMadePts = new List<vec3>(128);
 
-        //boundary variables
-        public bool isDriveThru;
+        public double createBndOffset;
+        public bool isBndBeingMade;
 
-        public void CalculateFenceLineHeadings()
+        public bool isDrawRightSide = true, isOkToAddPoints = false;
+
+        public int closestFenceNum;
+
+        //point at the farthest boundary segment from pivotAxle
+        public vec3 closestFencePt = new vec3(-10000, -10000, 9);
+
+        public bool IsPointInsideFenceArea(vec3 testPoint)
         {
-            //to calc heading based on next and previous points to give an average heading.
-            int cnt = fenceLine.Count;
-            vec3[] arr = new vec3[cnt];
-            cnt--;
-            fenceLine.CopyTo(arr);
-            fenceLine.Clear();
-
-            //first point needs last, first, second points
-            vec3 pt3 = arr[0];
-            pt3.heading = Math.Atan2(arr[1].easting - arr[cnt].easting, arr[1].northing - arr[cnt].northing);
-            if (pt3.heading < 0) pt3.heading += glm.twoPI;
-            fenceLine.Add(pt3);
-
-            //middle points
-            for (int i = 1; i < cnt; i++)
+            //first where are we, must be inside outer and outside of inner geofence non drive thru turn borders
+            if (bndList[0].fenceLineEar.IsPointInPolygon(testPoint))
             {
-                pt3 = arr[i];
-                pt3.heading = Math.Atan2(arr[i + 1].easting - arr[i - 1].easting, arr[i + 1].northing - arr[i - 1].northing);
-                if (pt3.heading < 0) pt3.heading += glm.twoPI;
-                fenceLine.Add(pt3);
+                for (int i = 1; i < bndList.Count; i++)
+                {
+                    //make sure not inside a non drivethru boundary
+                    //if (bndList[i].isDriveThru) continue;
+                    if (bndList[i].fenceLineEar.IsPointInPolygon(testPoint))
+                    {
+                        return false;
+                    }
+                }
+                //we are safely inside outer, outside inner boundaries
+                return true;
             }
-
-            //last and first point
-            pt3 = arr[cnt];
-            pt3.heading = Math.Atan2(arr[0].easting - arr[cnt - 1].easting, arr[0].northing - arr[cnt - 1].northing);
-            if (pt3.heading < 0) pt3.heading += glm.twoPI;
-            fenceLine.Add(pt3);
+            return false;
         }
 
-        public void FixFenceLine(int bndNum)
+        public bool IsPointInsideFenceArea(vec2 testPoint)
         {
-            double spacing;
-            //close if less then 20 ha, 40ha, more
-            if (area < 200000) spacing = 1.1;
-            else if (area < 400000) spacing = 2.2;
-            else spacing = 3.3;
-
-            if (bndNum > 0) spacing *= 0.5;
-
-            int bndCount = fenceLine.Count;
-            double distance;
-
-            //make sure distance isn't too big between points on boundary
-            for (int i = 0; i < bndCount; i++)
+            //first where are we, must be inside outer and outside of inner geofence non drive thru turn borders
+            if (bndList[0].fenceLineEar.IsPointInPolygon(testPoint))
             {
-                int j = i + 1;
-
-                if (j == bndCount) j = 0;
-                distance = glm.Distance(fenceLine[i], fenceLine[j]);
-                if (distance > spacing * 1.5)
+                for (int i = 1; i < bndList.Count; i++)
                 {
-                    vec3 pointB = new vec3((fenceLine[i].easting + fenceLine[j].easting) / 2.0,
-                        (fenceLine[i].northing + fenceLine[j].northing) / 2.0, fenceLine[i].heading);
-
-                    fenceLine.Insert(j, pointB);
-                    bndCount = fenceLine.Count;
-                    i--;
+                    //make sure not inside a non drivethru boundary
+                    //if (bndList[i].isDriveThru) continue;
+                    if (bndList[i].fenceLineEar.IsPointInPolygon(testPoint))
+                    {
+                        return false;
+                    }
                 }
+                //we are safely inside outer, outside inner boundaries
+                return true;
             }
-
-            //make sure distance isn't too big between points on boundary
-            bndCount = fenceLine.Count;
-
-            for (int i = 0; i < bndCount; i++)
-            {
-                int j = i + 1;
-
-                if (j == bndCount) j = 0;
-                distance = glm.Distance(fenceLine[i], fenceLine[j]);
-                if (distance > spacing * 1.6)
-                {
-                    vec3 pointB = new vec3((fenceLine[i].easting + fenceLine[j].easting) / 2.0,
-                        (fenceLine[i].northing + fenceLine[j].northing) / 2.0, fenceLine[i].heading);
-
-                    fenceLine.Insert(j, pointB);
-                    bndCount = fenceLine.Count;
-                    i--;
-                }
-            }
-
-            //make sure distance isn't too small between points on headland
-            spacing *= 0.9;
-            bndCount = fenceLine.Count;
-            for (int i = 0; i < bndCount - 1; i++)
-            {
-                distance = glm.Distance(fenceLine[i], fenceLine[i + 1]);
-                if (distance < spacing)
-                {
-                    fenceLine.RemoveAt(i + 1);
-                    bndCount = fenceLine.Count;
-                    i--;
-                }
-            }
-
-            //make sure headings are correct for calculated points
-            CalculateFenceLineHeadings();
-
-            double delta = 0;
-            fenceLineEar?.Clear();
-
-            for (int i = 0; i < fenceLine.Count; i++)
-            {
-                if (i == 0)
-                {
-                    fenceLineEar.Add(new vec2(fenceLine[i].easting, fenceLine[i].northing));
-                    continue;
-                }
-                delta += (fenceLine[i - 1].heading - fenceLine[i].heading);
-                if (Math.Abs(delta) > 0.005)
-                {
-                    fenceLineEar.Add(new vec2(fenceLine[i].easting, fenceLine[i].northing));
-                    delta = 0;
-                }
-            }
+            return false;
         }
 
-        public void ReverseWinding()
+        public void DrawFenceLines()
         {
-            //reverse the boundary
-            int cnt = fenceLine.Count;
-            vec3[] arr = new vec3[cnt];
-            cnt--;
-            fenceLine.CopyTo(arr);
-            fenceLine.Clear();
-            for (int i = cnt; i >= 0; i--)
+            if (!mf.mc.isOutOfBounds)
             {
-                arr[i].heading -= Math.PI;
-                if (arr[i].heading < 0) arr[i].heading += glm.twoPI;
-                fenceLine.Add(arr[i]);
+                GL.Color4(0,0,0,0.0); // change from (0,0,0,0.)
+                GL.LineWidth(0); // change from 6 to 0
+
+                for (int i = 0; i < bndList.Count; i++)
+                {
+                    bndList[i].fenceLineEar.DrawPolygon();
+                }
+
+                GL.Color4(0.031f, 0.024f, 0.847f, 1.0f); //change color boundary 
+                GL.LineWidth(mf.ABLine.lineWidth * 4); // change from 2 to 6 to lineWidth * 4
+
+                for (int i = 0; i < bndList.Count; i++)
+                {
+                    bndList[i].fenceLineEar.DrawPolygon();
+                }
             }
-        }
-
-        //obvious
-        public bool CalculateFenceArea(int idx)
-        {
-            int ptCount = fenceLine.Count;
-            if (ptCount < 1) return false;
-
-            area = 0;         // Accumulates area in the loop
-            int j = ptCount - 1;  // The last vertex is the 'previous' one to the first
-
-            for (int i = 0; i < ptCount; j = i++)
+            else
             {
-                area += (fenceLine[j].easting + fenceLine[i].easting) * (fenceLine[j].northing - fenceLine[i].northing);
-            }
+                GL.LineWidth(mf.ABLine.lineWidth * 4); // change from 3 to 4
+                GL.Color3(0.031f, 0.024f, 0.847f); //change boundary color if out of boundary
 
-            bool isClockwise = area >= 0;
-
-            area = Math.Abs(area / 2);
-
-            //make sure is clockwise for outer counter clockwise for inner
-            if ((idx == 0 && isClockwise) || (idx > 0 && !isClockwise))
-            {
-                ReverseWinding();
+                for (int i = 0; i < bndList.Count; i++)
+                {
+                    bndList[i].fenceLineEar.DrawPolygon();
+                }
             }
 
-            return isClockwise;
+            ////closest points  TooDoo
+            //GL.Color3(0.70f, 0.95f, 0.95f);
+            //GL.PointSize(6.0f);
+            //GL.Begin(PrimitiveType.Points);
+            //GL.Vertex3(mf.bnd.closestTurnPt.easting, mf.bnd.closestTurnPt.northing, 0);
+            //GL.End();
+
+            if (bndBeingMadePts.Count > 0)
+            {
+                //the boundary so far
+                vec3 pivot = mf.pivotAxlePos;
+                GL.LineWidth(mf.ABLine.lineWidth);
+                GL.Color3(0.031f, 0.024f, 0.847f); //change waring color if out of boundary 0.031f, 0.024f, 0.847f
+                GL.Begin(PrimitiveType.LineStrip);
+                for (int h = 0; h < bndBeingMadePts.Count; h++) GL.Vertex3(bndBeingMadePts[h].easting, bndBeingMadePts[h].northing, 0);
+                GL.Color3(0.295f, 0.972f, 0.290f); 
+                GL.Vertex3(bndBeingMadePts[0].easting, bndBeingMadePts[0].northing, 0);
+                GL.End();
+
+                //line from last point to pivot marker
+                GL.Color3(0.825f, 0.842f, 0.0f);
+                GL.Enable(EnableCap.LineStipple);
+                GL.LineStipple(1, 0x0700);
+                GL.Begin(PrimitiveType.LineStrip);
+                if (isDrawRightSide)
+                {
+                    GL.Vertex3(bndBeingMadePts[0].easting, bndBeingMadePts[0].northing, 0);
+
+                    GL.Vertex3(pivot.easting + (Math.Sin(pivot.heading - glm.PIBy2) * -createBndOffset),
+                            pivot.northing + (Math.Cos(pivot.heading - glm.PIBy2) * -createBndOffset), 0);
+                    GL.Vertex3(bndBeingMadePts[bndBeingMadePts.Count - 1].easting, bndBeingMadePts[bndBeingMadePts.Count - 1].northing, 0);
+                }
+                else
+                {
+                    GL.Vertex3(bndBeingMadePts[0].easting, bndBeingMadePts[0].northing, 0);
+
+                    GL.Vertex3(pivot.easting + (Math.Sin(pivot.heading - glm.PIBy2) * createBndOffset),
+                            pivot.northing + (Math.Cos(pivot.heading - glm.PIBy2) * createBndOffset), 0);
+                    GL.Vertex3(bndBeingMadePts[bndBeingMadePts.Count - 1].easting, bndBeingMadePts[bndBeingMadePts.Count - 1].northing, 0);
+                }
+                GL.End();
+                GL.Disable(EnableCap.LineStipple);
+
+                //boundary points
+                GL.Color3(0.0f, 0.95f, 0.95f);
+                GL.PointSize(6.0f);
+                GL.Begin(PrimitiveType.Points);
+                for (int h = 0; h < bndBeingMadePts.Count; h++) GL.Vertex3(bndBeingMadePts[h].easting, bndBeingMadePts[h].northing, 0);
+                GL.End();
+            }
         }
     }
 }
